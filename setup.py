4a6b46735e2752bd16db8bad5fa6a6c727469a23
Maxwell Dalboni <maxwell.dalboni@dieboldnixdorf.com>
2019-10-29 11:05:15 -0300

    feat:Fixed status validation

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from distutils.core import setup
 setup(
     name='reportportal_manager',
     packages=['reportportal_manager'],
-    version='1.0',
+    version='1.1',
     license='MIT',
     description='A simple abstraction for report portal library, for behave tests.',
     author='Maxwell Martins Dalboni',
